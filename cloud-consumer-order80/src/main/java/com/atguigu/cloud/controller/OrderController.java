d2876e1b54831165c6bf5148f4ce7a95c3ff5796
龚燕飞 <gongyanfei@bonc.com.cn>
2024-03-01 10:11:48 +0800

    cloud2024初始化，及consul的使用

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -0,0 +1,51 @@
+package com.atguigu.cloud.controller;
+
+
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+
+//    private static final String PaymentSrv_URL = "http://localhost:8001";
+    private static final String PaymentSrv_URL = "http://cloud-payment-service";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+
+
+    @PostMapping("/consumer/pay/add")
+    public ResultData<?> addOrder(@RequestBody PayDTO payDTO){
+        return restTemplate.postForObject(PaymentSrv_URL + "/pay/add",payDTO,ResultData.class);
+    }
+
+    @DeleteMapping("/consumer/pay/delete/{id}")
+    public ResultData<?> deleteOrder(@PathVariable("id") String id){
+
+        String url = PaymentSrv_URL + "/pay/delete/" + id;
+        HttpEntity<String> requestEntity = new HttpEntity<>("");
+        ResponseEntity<ResultData> response = restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, ResultData.class);
+        return response.getBody();
+    }
+    @GetMapping("/consumer/pay/get/{id}")
+    public ResultData<?> getPayInfo(@PathVariable("id") Integer id) {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/" + id, ResultData.class, id);
+
+    }
+
+    @PutMapping("/consumer/pay/update")
+    public ResultData<?> updataOrder(@RequestBody PayDTO payDTO){
+
+        String url = PaymentSrv_URL + "/pay/update";
+        HttpEntity<PayDTO> requestEntity = new HttpEntity<>(payDTO);
+        ResponseEntity<ResultData> response = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, ResultData.class);
+        return response.getBody();
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -0,0 +1,72 @@
+package com.atguigu.cloud.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@Tag(name="支付微服务模块",description = "支付CRUD")
+public class PayController {
+
+    @Resource
+    private PayService payService;
+
+
+    @PostMapping("/pay/add")
+    @Operation(summary = "新增",description = "新增支付流水方法，json串做参数")
+    public ResultData<?> addPay(@RequestBody Pay pay){
+        log.info(pay.toString());
+        int i = payService.add(pay);
+        return ResultData.sussess("成功插入记录，返回值："+i);
+    }
+
+    @DeleteMapping("/pay/delete/{id}")
+    @Operation(summary = "删除",description = "支付流水删除方法")
+    public ResultData<?> deletePay(@PathVariable("id") Integer id){
+        int i = payService.delete(id);
+        return ResultData.sussess("成功删除记录，返回值："+i);
+    }
+
+    @PutMapping("/pay/update")
+    @Operation(summary = "修改",description = "支付流水修改方法")
+    public ResultData<?> deletePay(@RequestBody PayDTO payDTO){
+        Pay pay = new Pay();
+        BeanUtil.copyProperties(payDTO,pay);
+        int i = payService.update(pay);
+        return ResultData.sussess("成功修改记录，返回值："+i);
+    }
+
+    @GetMapping("/pay/get/{id}")
+    @Operation(summary = "按ID流水查看",description = "支付流水查看方法")
+    public ResultData<?> getPay(@PathVariable("id") Integer id){
+        Pay pay = payService.select(id);
+        return ResultData.sussess(pay);
+    }
+
+    @GetMapping("/pay/getAll")
+    @Operation(summary = "查看所有流水",description = "支付流水查看所有流水方法")
+    public ResultData<?> getPay(){
+        List<Pay> pays = payService.selectAll();
+        return ResultData.sussess(pays);
+    }
+
+
+
+
+
+    @GetMapping("/pay/get/version")
+    public String getAtguiguVersion(@Value("${atguigu.version}") String atguiguVersion){
+        return atguiguVersion;
+    }
+}
