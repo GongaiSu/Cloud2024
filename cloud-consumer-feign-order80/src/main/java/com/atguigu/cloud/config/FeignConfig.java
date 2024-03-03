2288209d3b0e0b6cff5c96e5be52278244b28981
gyf <2533865444@qq.com>
2024-03-03 23:15:15 +0800

    openfeign开启传输压缩，开启日志

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,12 @@ public class FeignConfig {
 
     @Bean
     public Retryer retryer(){
-        return Retryer.NEVER_RETRY;
-//        return new Retryer.Default(100,1,3);
+//        return Retryer.NEVER_RETRY;
+        return new Retryer.Default(100,1,3);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
     }
 }
